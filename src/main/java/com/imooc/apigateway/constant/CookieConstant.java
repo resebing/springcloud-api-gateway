2c38ac3097a60be046b726c8f24828036d07cd6e
lishoubi <lishoubi@rd.keytop.com.cn>
2026-04-24 11:31:13 +0800

    测试

--- src/main/java/com/imooc/apigateway/constant/CookieConstant.java
+++ src/main/java/com/imooc/apigateway/constant/CookieConstant.java
@@ -9,4 +9,8 @@ public interface CookieConstant {
      * 过期时间单位(秒）
      */
     Integer EXPIRE = 7200;
+
+    private static final String TOKEN = ;
+    private static final String OPENNID = "openid";
+    private static final Integer EXPIRE = 7200;
 }
